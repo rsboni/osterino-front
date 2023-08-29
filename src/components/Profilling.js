3a5a7ccfbae52f0ee1c3a36a813d650e9b1dcbd1
Rafael da Silva Bonifácio <rsboni@gmail.com>
2023-08-28 23:30:18 -0300

    fix: better height

--- src/components/Profilling.js
+++ src/components/Profilling.js
@@ -2,31 +2,24 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
-// import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-// import Divider from '@mui/material/Divider';
-// import InboxIcon from '@mui/icons-material/Inbox';
-// import DraftsIcon from '@mui/icons-material/Drafts';
 import { profiles } from '../utils/profiles';
-import { profileMap } from '../utils/profileCalculator';
-import EspressoChart from './EspressoChart';
 import ProfileCard from './ProfileCard';
-
 export default function Profilling() {
   const [selectedIndex, setSelectedIndex] = React.useState(1);
 
   const handleListItemClick = (event, index) => {
     setSelectedIndex(index);
   };
-  return (<>
-    <Box sx={{ width: '40%', maxWidth: 360, height: '100%', bgcolor: 'background.paper' }}>
+  return (<Box sx={{display: 'flex', height:'85vh'}}>
+    <Box sx={{ width: '40%', overflow:'auto', maxWidth: 360, height: '100%', bgcolor: 'background.paper' }}>
       <List component="nav" aria-label="profile list" sx={{
         width: '100%',
         maxWidth: 360,
         bgcolor: 'background.paper',
         position: 'relative',
         overflow: 'auto',
-        maxHeight: '900px',
+        // maxHeight: '100vh',
         '& ul': { padding: 0 },
       }}>
         {profiles.map((profile, index) => {
@@ -41,9 +34,9 @@ export default function Profilling() {
         })}
       </List>
     </Box>
-    <Box sx={{ width: '60%', maxWidth: 900, bgcolor: 'background.paper' }}>
+    <Box sx={{ width: '60%', maxWidth: 900, bgcolor: 'background.paper',flexGrow: 1, p: 3, overflow:'auto' }}>
       <ProfileCard profile={profiles[selectedIndex]} />
     </Box>
-  </>
+  </Box>
   );
 }
